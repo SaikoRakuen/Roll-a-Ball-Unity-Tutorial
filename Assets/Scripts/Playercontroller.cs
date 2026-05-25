1ffa980a32ba2b6772d53e03be348a3488c37a9f
Saikon <saikorakuen@gmail.com>
2026-05-25 02:22:27 +0100

    Player movement

---
+++ Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Playercontroller : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementvalue)
+    {
+        Vector2 movementVector = movementvalue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement);
+    }
+
+   
+}
